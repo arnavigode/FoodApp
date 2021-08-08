d27b558d471420966a3437c9c0380c77052165cb
arnavigode <arnavi.gode96@gmail.com>
2021-08-08 17:59:10 +0530

    food app

---
+++ nav.js
@@ -0,0 +1,9 @@
+function navbar() {
+    return `<div id="navbar">
+        <h2><a href="search.html">Search by Name</a></h2>
+        <h2><a href="reipeofday.html">Receipe of Day</a></h2>
+        <h2><a href="latest.html">Lastest Receipe</a></h2>
+    </div>`
+}
+
+export default navbar
